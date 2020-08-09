29c067ddd56a618c92f6943a8a93ed0e1a033ea1
Anton <anton.avhutskiy@gmail.com>
2020-08-09 18:54:51 +0300

    Pochti End

--- src/js/script.js
+++ src/js/script.js
@@ -68,5 +68,26 @@ function opener(imgId){
 }
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu__item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger__active');
+      menu.classList.toggle('menu__active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger__active');
+          menu.classList.toggle('menu__active');
+      })
+  })
+})
